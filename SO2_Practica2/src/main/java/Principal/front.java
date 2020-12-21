e16ff2e7c6516efcaff94458999f7d6161c2bd70
esthuardo12 <esthuardo12@gmail.com>
2020-12-20 20:23:15 -0600

    Se agregó la estantería

--- SO2_Practica2/src/main/java/Principal/front.java
+++ SO2_Practica2/src/main/java/Principal/front.java
@@ -33,11 +33,11 @@ public class front extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 501, Short.MAX_VALUE)
+            .addGap(0, 551, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 375, Short.MAX_VALUE)
+            .addGap(0, 403, Short.MAX_VALUE)
         );
 
         pack();
